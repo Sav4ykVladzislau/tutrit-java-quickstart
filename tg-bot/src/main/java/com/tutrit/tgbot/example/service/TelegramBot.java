d97cac6a80142b293ae4f4df553a3bcf2df5c5f0
Sav4ykVladzislau <44sav4yk@gmail.com>
2022-06-13 18:13:59 +0300

    My first commit in GitHub

--- tg-bot/src/main/java/com/tutrit/tgbot/example/service/TelegramBot.java
+++ tg-bot/src/main/java/com/tutrit/tgbot/example/service/TelegramBot.java
@@ -1,17 +1,15 @@
 package com.tutrit.tgbot.example.service;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-@Component
-@PropertySource("application.properties")
+@Service
 public class TelegramBot extends TelegramLongPollingBot {
-    @Value("${bot.username}")
+    @Value( "${bot.username}")
     private String botUsername;
     @Value("${bot.token}")
     private String botToken;
